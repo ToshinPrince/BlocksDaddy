1fad82f47d089d20c720e885930afa74f7f886e9
Toshin Prince <toshinprince@gmail.com>
2023-08-18 12:21:59 +0530

    Contract_initiated

--- contracts/BlocksDaddy.sol
+++ contracts/BlocksDaddy.sol
@@ -1,8 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract BlocksDaddy {
-    string public name = "BlocksDaddy";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-    constructor() public {}
+contract BlocksDaddy is ERC721 {
+    address public owner;
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {
+        owner = msg.sender;
+    }
 }
--- test/BlocksDaddy.js
+++ test/BlocksDaddy.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
@@ -6,15 +7,35 @@ const tokens = (n) => {
 
 describe("BlocksDaddy", () => {
   let blocksDaddy;
+  let deployer, owner1;
+
+  const NAME = "BlocksDaddy";
+  const SYMBOL = "BD";
 
   beforeEach(async () => {
+    //setting Accounts
+    [deployer, owner1] = await ethers.getSigners();
+
+    //Deploploying Contract
     const BlocksDaddy = await ethers.getContractFactory("BlocksDaddy");
-    blocksDaddy = await BlocksDaddy.deploy();
+    blocksDaddy = await BlocksDaddy.deploy("BlocksDaddy", "BD");
     // await blocksDaddy.wait;
   });
 
-  it("Has a name", async () => {
-    const result = await blocksDaddy.name();
-    expect(result).to.be.equal("BlocksDaddy");
+  describe("deployment", () => {
+    it("Has a name", async () => {
+      const result = await blocksDaddy.name();
+      expect(result).to.be.equal(NAME);
+    });
+
+    it("Has a Symbol", async () => {
+      const result = await blocksDaddy.symbol();
+      expect(result).to.be.equal(SYMBOL);
+    });
+
+    it("Deployer is owner", async () => {
+      const result = await blocksDaddy.owner();
+      expect(result).to.be.equal(deployer.address);
+    });
   });
 });
