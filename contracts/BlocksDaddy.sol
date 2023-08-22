d1da2e14075359d87382c33a07d6e1d099c0a7bb
Toshin Prince <toshinprince@gmail.com>
2023-08-22 13:21:04 +0530

    mint

--- contracts/BlocksDaddy.sol
+++ contracts/BlocksDaddy.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BlocksDaddy is ERC721 {
     uint256 public maxSupply;
+    uint256 public totalSupply;
 
     address public owner;
 
@@ -33,7 +34,22 @@ contract BlocksDaddy is ERC721 {
         domains[maxSupply] = Domain(_name, _cost, false);
     }
 
+    function mint(uint256 _id) public payable {
+        require(_id != 0);
+        require(_id <= maxSupply);
+        require(domains[_id].isOwned == false);
+        require(msg.value >= domains[_id].cost);
+
+        domains[_id].isOwned = true;
+        totalSupply++;
+        _safeMint(msg.sender, _id);
+    }
+
     function getDomain(uint256 _id) public view returns (Domain memory) {
         return (domains[_id]);
+    }
+
+    function getBalance() public view returns (uint256) {
+        return (address(this).balance);
     }
 }
