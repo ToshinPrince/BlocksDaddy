23cc14d24de5e8fce1037cd6d883ecb0c2d34dc1
Toshin Prince <toshinprince@gmail.com>
2023-08-24 12:31:24 +0530

    deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,13 +5,58 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const { ethers } = require("ethers");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 async function main() {
+  const NAME = "BlocksDaddy";
+  const SYMBOL = "BD";
 
+  //setting up account
+  const [deployer] = await hre.ethers.getSigners();
+
+  //Deploing Contract
+  const BlocksDaddy = await hre.ethers.getContractFactory("BlocksDaddy");
+  const blocksDaddy = await BlocksDaddy.deploy(NAME, SYMBOL);
+  await blocksDaddy.deployed();
+
+  //listing 10 domain names
+  const names = [
+    "alice.eth",
+    "bob.eth",
+    "charlie.eth",
+    "dave.eth",
+    "eve.eth",
+    "frank.eth",
+    "grace.eth",
+    "helen.eth",
+    "irene.eth",
+    "jack.eth",
+  ];
+
+  const costs = [
+    tokens(1.2),
+    tokens(7),
+    tokens(2),
+    tokens(1.5),
+    tokens(1),
+    tokens(2.5),
+    tokens(1.8),
+    tokens(3),
+    tokens(8),
+    tokens(1.4),
+  ];
+
+  for (var i = 0; i < 10; i++) {
+    const transaction = await blocksDaddy
+      .connect(deployer)
+      .list(names[i], costs[i]);
+
+    console.log(`Listed Domain Name ${i + 1}: ${names[i]}`);
+  }
 }
 
 main().catch((error) => {
