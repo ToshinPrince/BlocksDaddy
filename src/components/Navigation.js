d16891b9b416949d8823e482c8f2b00c084f5c05
Toshin Prince <toshinprince@gmail.com>
2023-08-28 10:24:42 +0530

    listing_progress

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,11 +2,19 @@ import { ethers } from "ethers";
 import logo from "../assets/logo.svg";
 
 const Navigation = ({ account, setAccount }) => {
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const account = ethers.utils.getAddress(accounts[0]);
+    setAccount(account);
+  };
+
   return (
     <nav>
       <div className="nav__brand">
         <img src={logo} alt="Logo" />
-        <h1>ETH Daddy</h1>
+        <h1>BLOCKS Daddy</h1>
 
         <ul className="nav__links">
           <li>
@@ -23,7 +31,15 @@ const Navigation = ({ account, setAccount }) => {
           </li>
         </ul>
       </div>
-      <button>{account.slice(0, 6)}</button>
+      {account ? (
+        <button type="button" className="nav__connect">
+          {account.slice(0, 6) + "..." + account.slice(38, 42)}
+        </button>
+      ) : (
+        <button type="button" className="nav__connect" onClick={connectHandler}>
+          Connect
+        </button>
+      )}
     </nav>
   );
 };
